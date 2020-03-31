9ab001f7237a9d64f630c6736eb18effdf5e8fac
Bryan <bryanjcs1@gmail.com>
2020-03-31 15:31:01 -0400

    31/03/2020

--- Models/EmployeeDataAccessLayer.cs
+++ Models/EmployeeDataAccessLayer.cs
@@ -10,8 +10,91 @@ namespace mvc_test.Models
     public class EmployeeDataAccessLayer     
     {    
         string connectionString = "Data Source=DESKTOP-DPPC2T9;Database=master;Integrated Security=TRUE";    
+     
+        public void AddClientes(Clientes clientes)    
+        {    
+            using (SqlConnection con = new SqlConnection(connectionString))    
+            {    
+                SqlCommand cmd = new SqlCommand("spAddClientes", con);    
+                cmd.CommandType = CommandType.StoredProcedure;    
     
-        //To View all employees details      
+                cmd.Parameters.AddWithValue("@nombre", clientes.nombre);    
+                cmd.Parameters.AddWithValue("@apellido", clientes.apellido);    
+                cmd.Parameters.AddWithValue("@documento", clientes.documento);    
+                cmd.Parameters.AddWithValue("@fecha_nacimiento", clientes.fecha_nacimiento);    
+    
+                con.Open();    
+                cmd.ExecuteNonQuery();    
+                con.Close();    
+            }    
+        }
+        
+        public void UpdateClientes(Clientes clientes)    
+        {    
+            using (SqlConnection con = new SqlConnection(connectionString))    
+            {    
+                SqlCommand cmd = new SqlCommand("spUpdateCliente", con);    
+                cmd.CommandType = CommandType.StoredProcedure;    
+
+                cmd.Parameters.AddWithValue("@ClieId", clientes.ID);        
+                cmd.Parameters.AddWithValue("@Nombre", clientes.nombre);    
+                cmd.Parameters.AddWithValue("@Apellido", clientes.apellido);    
+                cmd.Parameters.AddWithValue("@documento", clientes.documento);    
+                cmd.Parameters.AddWithValue("@fecha_nacimiento", clientes.fecha_nacimiento);    
+    
+                con.Open();    
+                cmd.ExecuteNonQuery();    
+                con.Close();    
+            }    
+        }
+
+        public Clientes GetClientData(int? id)    
+        {    
+            Clientes Cliente = new Clientes();// ver como usar un patron de disenho con las instanciaciones    
+    
+            using (SqlConnection con = new SqlConnection(connectionString))    
+            {    
+                string sqlQuery = "SELECT * FROM Clientes WHERE ID= " + id;    
+                SqlCommand cmd = new SqlCommand(sqlQuery, con);    
+    
+                con.Open();    
+                SqlDataReader rdr = cmd.ExecuteReader();    
+    
+                while (rdr.Read())    
+                {    
+                    Cliente.ID = Convert.ToInt32(rdr["ID"]);    
+                    Cliente.nombre = rdr["nombre"].ToString();    
+                    Cliente.apellido = rdr["apellido"].ToString();
+                    Cliente.documento = rdr["documento"].ToString();     
+                    Cliente.fecha_nacimiento = DateTime.Parse(rdr["fecha_nacimiento"].ToString());    
+                    
+                }    
+            }    
+            return Cliente;    
+        }    
+        
+        //To Delete the record on a particular employee    
+        public void DeleteCliente(int? id)    
+        {    
+    
+            using (SqlConnection con = new SqlConnection(connectionString))    
+            {    
+                SqlCommand cmd = new SqlCommand("spDeleteCliente", con);    
+                cmd.CommandType = CommandType.StoredProcedure;    
+    
+                cmd.Parameters.AddWithValue("@ClieId", id);    
+    
+                con.Open();    
+                cmd.ExecuteNonQuery();    
+                con.Close();    
+            }    
+        }    
+        
+    
+    
+        #region  Employee
+
+         //To View all employees details      
         public IEnumerable<Employee> GetAllEmployees()    
         {    
             List<Employee> lstemployee = new List<Employee>();    
@@ -70,87 +153,6 @@ namespace mvc_test.Models
             return lstClientes;    
         }
             
-        
-        public void AddClientes(Clientes clientes)    
-        {    
-            using (SqlConnection con = new SqlConnection(connectionString))    
-            {    
-                SqlCommand cmd = new SqlCommand("spAddClientes", con);    
-                cmd.CommandType = CommandType.StoredProcedure;    
-    
-                cmd.Parameters.AddWithValue("@nombre", clientes.nombre);    
-                cmd.Parameters.AddWithValue("@apellido", clientes.apellido);    
-                cmd.Parameters.AddWithValue("@documento", clientes.documento);    
-                cmd.Parameters.AddWithValue("@fecha_nacimiento", clientes.fecha_nacimiento);    
-    
-                con.Open();    
-                cmd.ExecuteNonQuery();    
-                con.Close();    
-            }    
-        }
-        
-        public void UpdateClientes(Clientes clientes)    
-        {    
-            using (SqlConnection con = new SqlConnection(connectionString))    
-            {    
-                SqlCommand cmd = new SqlCommand("spUpdateCliente", con);    
-                cmd.CommandType = CommandType.StoredProcedure;    
-
-                cmd.Parameters.AddWithValue("@ClieId", clientes.ID);        
-                cmd.Parameters.AddWithValue("@Nombre", clientes.nombre);    
-                cmd.Parameters.AddWithValue("@Apellido", clientes.apellido);    
-                cmd.Parameters.AddWithValue("@documento", clientes.documento);    
-                cmd.Parameters.AddWithValue("@fecha_nacimiento", clientes.fecha_nacimiento);    
-    
-                con.Open();    
-                cmd.ExecuteNonQuery();    
-                con.Close();    
-            }    
-        }
-
-        public Clientes GetClientData(int? id)    
-        {    
-            Clientes Cliente = new Clientes();    
-    
-            using (SqlConnection con = new SqlConnection(connectionString))    
-            {    
-                string sqlQuery = "SELECT * FROM Clientes WHERE ID= " + id;    
-                SqlCommand cmd = new SqlCommand(sqlQuery, con);    
-    
-                con.Open();    
-                SqlDataReader rdr = cmd.ExecuteReader();    
-    
-                while (rdr.Read())    
-                {    
-                    Cliente.ID = Convert.ToInt32(rdr["ID"]);    
-                    Cliente.nombre = rdr["nombre"].ToString();    
-                    Cliente.apellido = rdr["apellido"].ToString();
-                    Cliente.documento = rdr["documento"].ToString();     
-                    Cliente.fecha_nacimiento = DateTime.Parse(rdr["fecha_nacimiento"].ToString());    
-                    
-                }    
-            }    
-            return Cliente;    
-        }    
-        
-        //To Delete the record on a particular employee    
-        public void DeleteCliente(int? id)    
-        {    
-    
-            using (SqlConnection con = new SqlConnection(connectionString))    
-            {    
-                SqlCommand cmd = new SqlCommand("spDeleteCliente", con);    
-                cmd.CommandType = CommandType.StoredProcedure;    
-    
-                cmd.Parameters.AddWithValue("@ClieId", id);    
-    
-                con.Open();    
-                cmd.ExecuteNonQuery();    
-                con.Close();    
-            }    
-        }    
-        
-        #region  Employee
         //To Update the records of a particluar employee    
         public void UpdateEmployee(Employee employee)    
         {    
