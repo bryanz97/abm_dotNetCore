4b7b0db9da3ff1d287f1201a48c3b0f9e091f252
Bryan <bryanjcs1@gmail.com>
2020-03-31 15:44:38 -0400

    eliminamos clase employee

--- Models/EmployeeDataAccessLayer.cs
+++ Models/EmployeeDataAccessLayer.cs
@@ -91,39 +91,7 @@ namespace mvc_test.Models
         }    
         
     
-    
-        #region  Employee
 
-         //To View all employees details      
-        public IEnumerable<Employee> GetAllEmployees()    
-        {    
-            List<Employee> lstemployee = new List<Employee>();    
-    
-            using (SqlConnection con = new SqlConnection(connectionString))    
-            {    
-                SqlCommand cmd = new SqlCommand("spGetAllEmployees", con);    
-                cmd.CommandType = CommandType.StoredProcedure;    
-    
-                con.Open();    
-                SqlDataReader rdr = cmd.ExecuteReader();    
-    
-                while (rdr.Read())    
-                {    
-                    Employee employee = new Employee();    
-    
-                    employee.ID = Convert.ToInt32(rdr["EmployeeID"]);    
-                    employee.Name = rdr["Name"].ToString();    
-                    employee.Gender = rdr["Gender"].ToString();    
-                    employee.Department = rdr["Department"].ToString();    
-                    employee.City = rdr["City"].ToString();    
-    
-                    lstemployee.Add(employee);    
-                }    
-                con.Close();    
-            }    
-            return lstemployee;    
-        }    
-    
         public IEnumerable<Clientes> GetAllClientes()    
         {    
             List<Clientes> lstClientes = new List<Clientes>();    
@@ -151,88 +119,7 @@ namespace mvc_test.Models
                 con.Close();    
             }    
             return lstClientes;    
-        }
-            
-        //To Update the records of a particluar employee    
-        public void UpdateEmployee(Employee employee)    
-        {    
-            using (SqlConnection con = new SqlConnection(connectionString))    
-            {    
-                SqlCommand cmd = new SqlCommand("spUpdateEmployee", con);    
-                cmd.CommandType = CommandType.StoredProcedure;    
-    
-                cmd.Parameters.AddWithValue("@EmpId", employee.ID);    
-                cmd.Parameters.AddWithValue("@Name", employee.Name);    
-                cmd.Parameters.AddWithValue("@Gender", employee.Gender);    
-                cmd.Parameters.AddWithValue("@Department", employee.Department);    
-                cmd.Parameters.AddWithValue("@City", employee.City);    
-    
-                con.Open();    
-                cmd.ExecuteNonQuery();    
-                con.Close();    
-            }    
-        }
-
-        //Get the details of a particular employee    
-        public Employee GetEmployeeData(int? id)    
-        {    
-            Employee employee = new Employee();    
-    
-            using (SqlConnection con = new SqlConnection(connectionString))    
-            {    
-                string sqlQuery = "SELECT * FROM tblEmployee WHERE EmployeeID= " + id;    
-                SqlCommand cmd = new SqlCommand(sqlQuery, con);    
-    
-                con.Open();    
-                SqlDataReader rdr = cmd.ExecuteReader();    
-    
-                while (rdr.Read())    
-                {    
-                    employee.ID = Convert.ToInt32(rdr["EmployeeID"]);    
-                    employee.Name = rdr["Name"].ToString();    
-                    employee.Gender = rdr["Gender"].ToString();    
-                    employee.Department = rdr["Department"].ToString();    
-                    employee.City = rdr["City"].ToString();    
-                }    
-            }    
-            return employee;    
-        }    
-    
-        //To Delete the record on a particular employee    
-        public void DeleteEmployee(int? id)    
-        {    
-    
-            using (SqlConnection con = new SqlConnection(connectionString))    
-            {    
-                SqlCommand cmd = new SqlCommand("spDeleteEmployee", con);    
-                cmd.CommandType = CommandType.StoredProcedure;    
-    
-                cmd.Parameters.AddWithValue("@EmpId", id);    
-    
-                con.Open();    
-                cmd.ExecuteNonQuery();    
-                con.Close();    
-            }    
-        } 
-
-        //To Add new employee record      
-        public void AddEmployee(Employee employee)    
-        {    
-            using (SqlConnection con = new SqlConnection(connectionString))    
-            {    
-                SqlCommand cmd = new SqlCommand("spAddEmployee", con);    
-                cmd.CommandType = CommandType.StoredProcedure;    
-    
-                cmd.Parameters.AddWithValue("@Name", employee.Name);    
-                cmd.Parameters.AddWithValue("@Gender", employee.Gender);    
-                cmd.Parameters.AddWithValue("@Department", employee.Department);    
-                cmd.Parameters.AddWithValue("@City", employee.City);    
-    
-                con.Open();    
-                cmd.ExecuteNonQuery();    
-                con.Close();    
-            }    
         }   
-        #endregion
+       
     }    
 }    
